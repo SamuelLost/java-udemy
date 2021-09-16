f79e0bf8c33e883406acb78ec8fd37217ec379fd
SamuelLost <samuelhenriq12@gmail.com>
2021-09-16 11:54:44 -0300

    Mais exercícios

--- src/Exerc_PARTE2/Exerc5.java
+++ src/Exerc_PARTE2/Exerc5.java
@@ -1,10 +1,41 @@
 package Exerc_PARTE2;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Exerc5 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		int codigo, qntd;
+		double valor=0;
+		codigo = sc.nextInt();
+		qntd = sc.nextInt();
+		
+		if(codigo == 1) {
+			valor = qntd*4;
+		}
+		else if(codigo == 2) {
+			valor = qntd*4.5;
+		}
+		else if(codigo == 3) {
+			valor = qntd*5;
+		}
+		else if(codigo == 4) {
+			valor = qntd*2;
+		}
+		else if(codigo == 5){
+			valor = qntd*1.5;
+		}
+		else {
+			System.out.println("Opção inválida");
+			System.exit(0);
+		}
+		
+		System.out.printf("Total: R$ %.2f", valor);
+		
+		sc.close();
 	}
 
 }

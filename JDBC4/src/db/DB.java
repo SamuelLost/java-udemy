a305c582ad72a2a2d8492cc5afc0abe9eda8d33b
Samuel Henrique <samuelhenriqec@alu.ufc.br>
2023-01-24 15:59:59 -0300

    Deletando dados

---
+++ JDBC4/src/db/DB.java
@@ -0,0 +1,68 @@
+package db;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+public class DB {
+
+	private static Connection conn = null;
+
+	public static Connection getConnection() {
+		if (conn == null) {
+			try {
+				Properties ps = loadProperties();
+				String url = ps.getProperty("dburl");
+				conn = DriverManager.getConnection(url, ps);
+			} catch (SQLException e) {
+				throw new DbExeception(e.getMessage());
+			}
+		}
+		return conn;
+	}
+
+	public static void closeConnection() {
+		if (conn != null) {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				throw new DbExeception(e.getMessage());
+			}
+		}
+	}
+
+	private static Properties loadProperties() {
+		try (FileInputStream fs = new FileInputStream("db.properties")) {
+			Properties ps = new Properties();
+			ps.load(fs);
+			return ps;
+		} catch (IOException e) {
+			throw new DbExeception(e.getMessage());
+		}
+	}
+
+	public static void closeStatement(Statement st) {
+		if (st != null) {
+			try {
+				st.close();
+			} catch (SQLException e) {
+				throw new DbExeception(e.getMessage());
+			}
+		}
+	}
+
+	public static void closeResultSet(ResultSet rs) {
+		if (rs != null) {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				throw new DbExeception(e.getMessage());
+			}
+		}
+	}
+}
